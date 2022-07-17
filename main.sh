fa3c917596243f48d6cfd97f6fc0e88a4fff100a
life <108618401+life00@users.noreply.github.com>
2022-07-17 11:08:35 +0000

    minor fixes

--- main.sh
+++ main.sh
@@ -7,10 +7,10 @@ TARGET=/
 # what is the interval of snapshots deletion
 INTERVAL=10
 # how the snapshot names would look like using GNU date
-DISPLAY="%m.%d.%y-%H:%M:%S"
+DISPLAY="%Y.%m.%d-%H:%M:%S"
 
 # optional logging
-proclog () {
+proclog() {
 if [[ "$1" == "delete+create" ]]; then
 	printf "tiny photographer: deleted '$old'\n"
 	printf "tiny photographer: created '$STORAGE$time'\n"
@@ -26,37 +26,36 @@ fi
 main() {
 	# gets the amount of snapshots
 	# something may break if in $STORAGE would be non snapshots
-	count=$(ls $STORAGE | wc -l)
+	count=$(ls "$STORAGE" | wc -l)
 	
 	# this is specifically for names
-	time=$(date +${DISPLAY})
+	time=$(date +"$DISPLAY")
 
-	if (( $count >= $INTERVAL )); then
+	if (( count >= INTERVAL )); then
 		# finds the oldest snapshot and deletes it
-		for f in $STORAGE*; do if [[ ! $old || $f -ot $old ]]; then old=$f; fi; done		
-		btrfs su de $old/snapshot
-		rmdir $old
+		for f in "$STORAGE"/*/; do if [[ ! "$old" || "$f" -ot "$old" ]]; then old="$f"; fi; done		
+		btrfs su de "$old"/snapshot
+		rmdir "$old"
 
 		# creates a new snapshot and logs actions
-		mkdir $STORAGE$time # additional directory is required to fix broken snapshot timestamps
-		btrfs su sn $TARGET $STORAGE$time/snapshot
-		btrfs pr set $STORAGE$time/snapshot ro true # optionally gives it read only permissions
+		mkdir "$STORAGE"/"$time" # additional directory is required to fix broken snapshot timestamps
+		btrfs su sn "$TARGET" "$STORAGE"/"$time"/snapshot
+		btrfs pr set "$STORAGE"/"$time"/snapshot ro true # optionally gives it read only permissions
 		proclog delete+create
 	else
 		# creates a new snapshot and logs actions
-		mkdir $STORAGE$time
-		btrfs su sn $TARGET $STORAGE$time/snapshot
-		btrfs pr set $STORAGE$time/snapshot ro true # optionally gives it read only permissions
+		mkdir "$STORAGE"/"$time"
+		btrfs su sn "$TARGET" "$STORAGE"/"$time"/snapshot
+		btrfs pr set "$STORAGE"/"$time"/snapshot ro true # optionally gives it read only permissions
 		proclog create
 	fi
 }
-
 # pause feature
 # create anything with "stop" in the $STORAGE
 # to stop tiny photographer
-if ! [[ $(ls $STORAGE | grep stop) != "" ]]; then
-	main
+if ls "$STORAGE" | grep -q stop; then
+        proclog stopped
+        exit
 else
-	proclog stopped
-	exit
+        main
 fi
